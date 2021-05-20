e7e185ad82930fa971fda2f1f5dd93546126f431
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-21 00:39:25 +0200

    initial commit

---
+++ src/main/java/pl/miloszlewandowski/dietly_api/DietlyApiApplication.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.dietly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietlyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietlyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/miloszlewandowski/dietly_api/DietlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.dietly_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietlyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
