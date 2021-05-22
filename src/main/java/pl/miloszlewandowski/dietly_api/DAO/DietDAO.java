255a245fb4dbdf4f522a0d1581a06af2615b2e06
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-22 20:28:08 +0200

    dao classes

---
+++ src/main/java/pl/miloszlewandowski/dietly_api/DAO/DietDAO.java
@@ -0,0 +1,9 @@
+package pl.miloszlewandowski.dietly_api.DAO;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.miloszlewandowski.dietly_api.model.Diet;
+
+
+public class DietDAO {
+
+}
---
+++ src/main/java/pl/miloszlewandowski/dietly_api/DAO/DietOptionDAO.java
@@ -0,0 +1,27 @@
+package pl.miloszlewandowski.dietly_api.DAO;
+
+import org.springframework.stereotype.Repository;
+import pl.miloszlewandowski.dietly_api.model.Diet;
+import pl.miloszlewandowski.dietly_api.model.DietOption;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+//@Repository
+//public class DietOptionDAO {
+//
+//    public static Set<DietOption> dietOptions = new HashSet<>();
+//
+//    public DietOptionDAO(Integer dietId){
+//
+//        for (int i = 1; i <= 5; i++) {
+//            DietOption dietOption = new DietOption();
+//            dietOption.setDietOptionId(i);
+//            dietOption.setName("Diet Option " + i);
+//            dietOption.setDiet();
+//            dietOption.setAbbreviation("Option " + i);
+//            dietOptions.add(dietOption);
+//        }
+//    }
+//}
