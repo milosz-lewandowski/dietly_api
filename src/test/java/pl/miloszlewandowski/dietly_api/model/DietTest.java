adfdae3df8fae0fd63da4f9263c3b8f290abd30f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-22 20:28:42 +0200

    tests

---
+++ src/test/java/pl/miloszlewandowski/dietly_api/model/DietTest.java
@@ -0,0 +1,24 @@
+package pl.miloszlewandowski.dietly_api.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DietTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDietOptions() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDietOptions() {
+    }
+}
