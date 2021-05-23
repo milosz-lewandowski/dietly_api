795474e4c30b4b4a5fb95fa89e31917893e47c0a
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-23 23:59:13 +0200

    change of endpoint response datatype

--- src/main/java/pl/miloszlewandowski/dietly_api/controller/DietController.java
+++ src/main/java/pl/miloszlewandowski/dietly_api/controller/DietController.java
@@ -1,26 +1,41 @@
 package pl.miloszlewandowski.dietly_api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.miloszlewandowski.dietly_api.model.Diet;
 import pl.miloszlewandowski.dietly_api.repository.DietRepository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-@RestController("/diets")
+@RestController
 public class DietController {
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
     DietRepository dietRepository;
 
-    @GetMapping("/")
-    public Set<Diet> getDietList() {
-        return dietRepository.getDiets();
+    @GetMapping("/diets")
+    public Map<String, Set<Diet>> getDietList() {
+        Set<Diet> diets = dietRepository.getDiets();
+        Map<String, Set<Diet>> responseData= new HashMap<>();
+        responseData.put("diets", diets);
+        return responseData;
     }
 
     @GetMapping("/{id}")
-    public Diet getDiet(@PathVariable Integer id) {
-        return dietRepository.getDietById(id);
+    public Map<String, Diet> getDiet(@PathVariable Integer id) {
+        Diet diet1 = new Diet();
+        diet1.setDietId(1);
+        diet1.setName("diet1");
+        diet1.setDescription("dieta 1");
+        dietRepository.saveDiet(diet1);
+        Map<String, Diet> responseData = new HashMap<>();
+        responseData.put("diet", dietRepository.getDietById(id));
+        return responseData;
     }
 
     @PostMapping("/save")
