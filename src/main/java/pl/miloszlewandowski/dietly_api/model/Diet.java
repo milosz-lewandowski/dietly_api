585ef6f7d6062bd761ad4e55512bdaa9ee09d5b8
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-24 23:46:20 +0200

    Diet model

--- src/main/java/pl/miloszlewandowski/dietly_api/model/Diet.java
+++ src/main/java/pl/miloszlewandowski/dietly_api/model/Diet.java
@@ -1,15 +1,12 @@
 package pl.miloszlewandowski.dietly_api.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Diet { //typ diety, np. wegetariańska, sportowa
     private Integer dietId;
     private String name;
